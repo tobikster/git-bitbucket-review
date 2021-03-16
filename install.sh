af4fefa03f8cb5c4bf294ad5ffce92db52ef698a
Paweł Tobiszewski <pawel.tobiszewski@gmail.com>
2021-03-16 20:59:53 +0100

    Add scripts for linking and unlinking the fish completion

---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ln ${PWD}/completion/git-bitbucket.fish ${HOME}/.config/fish/completions/git-bitbucket.fish && echo "Fish completion script linked successfully!"
---
+++ uninstall.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+unlink ${HOME}/.config/fish/completions/git-bitbucket.fish
